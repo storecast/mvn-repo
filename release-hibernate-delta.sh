5804cfba8e4973ff30c80e32022d781f5d00e0a4
Gregor Zeitlinger <gregor.zeitlinger@txtr.com>
2013-06-19 12:56:59 +0200

    release hibernate-delta

---
+++ release-hibernate-delta.sh
@@ -0,0 +1,1 @@
+mvn deploy:deploy-file -DgroupId=com.txtr -DartifactId=hibernate-delta -Dversion=0.1 -Dpackaging=jar -Dfile="/home/gregor/source/hibernate-delta/target/hibernate-delta-0.1.jar" -Durl=file:/home/gregor/source/txtr-mvn-repo
