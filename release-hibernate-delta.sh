384b29bca4d3b950a94b6d9dd1ea43cd374c0f94
Gregor Zeitlinger <gregor.zeitlinger@txtr.com>
2013-06-25 11:14:50 +0200

    new release

--- release-hibernate-delta.sh
+++ release-hibernate-delta.sh
@@ -1,1 +1,1 @@
-mvn deploy:deploy-file -DgroupId=com.txtr -DartifactId=hibernate-delta -Dversion=0.2 -Dpackaging=jar -Dfile="/home/gregor/source/hibernate-delta/target/hibernate-delta-0.2.jar" -Durl=file:/home/gregor/source/txtr-mvn-repo
+mvn deploy:deploy-file -DgroupId=com.txtr -DartifactId=hibernate-delta -Dversion=0.3 -Dpackaging=jar -Dfile="/home/gregor/source/hibernate-delta/target/hibernate-delta-0.3.jar" -Durl=file:/home/gregor/source/txtr-mvn-repo
